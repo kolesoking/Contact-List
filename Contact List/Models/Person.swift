f028b629ec2d3404e40eb62eb7084276f622aad9
Sergey <kolesnikov.sergey.al@gmail.com>
2022-04-02 12:35:15 +0300

    moved the DataManager class to a separate data model

--- Contact List/Models/Person.swift
+++ Contact List/Models/Person.swift
@@ -40,35 +40,3 @@ struct Person {
         return persons
     }
 }
-
-
-
-class DataManage {
-    let names = [
-        "Mary", "Lawrence", "Laura","Justin", "Marilyn",
-        "Betty", "Gene","Marcus","Richard", "Paul"
-    ]
-    
-    let surnames = [
-        "Chapman", "Norton", "Gaines", "Cain", "Wilson",
-        "Sherman", "Benson", "Holt", "Pearson", "Cooper"
-    ]
-    
-    let phones = [
-        "7(495)878-88-27", "7(495)535-58-63",
-        "7(495)098-20-93", "7(495)838-65-45",
-        "7(495)132-05-01", "7(495)366-23-06",
-        "7(495)056-05-09", "7(495)389-93-54",
-        "7(495)300-90-14", "7(495)001-23-12"
-        
-    ]
-    
-    let emails = [
-        "o@outlook.com", "hr6zdl@yandex.ru",
-        "kaft93x@outlook.com", "dcu@yandex.ru",
-        "19dn@outlook.com", "pa5h@mail.ru",
-        "281av0@gmail.com", "8edmfh@outlook.com",
-        "sfn13i@mail.ru", "g0orc3x1@outlook.com"
-    ]
-}
-
